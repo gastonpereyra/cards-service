b5b865535f4eeef1c2ea06cfbe3ff4a193204467
gastonpereyra <rgpxen@gmail.com>
2021-10-03 14:05:08 -0300

    Added Fetchers

---
+++ src/controllers/github-request/base.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const axios = require('axios');
+
+const ERROR_RATE_LIMITED = 'RATE_LIMITED';
+const ERROR_BAD_CREDENTIALS = 'Bad credentials';
+
+module.exports = class GithubFetcherBase {
+
+	static get url() {
+		return 'https://api.github.com/graphql';
+	}
+
+	static get method() {
+		return 'POST';
+	}
+
+	static get authorization() {
+		return `bearer ${process.env.GITHUB_TOKEN}`;
+	}
+
+	static async get(username) {
+
+		const invalidRequest = this.validateRequest();
+
+		if(invalidRequest)
+			return invalidRequest;
+
+		try {
+			const response = await axios(this.formatRequest(username));
+
+			const invalidResponse = this.validateResponse(response);
+
+			if(invalidResponse)
+				return invalidResponse;
+
+			return this.format(response.data);
+
+		} catch(error) {
+
+			if(error?.response?.data?.message === ERROR_BAD_CREDENTIALS)
+				return { error: 'Invalid Crendentials' };
+
+			return { error: error.message };
+		}
+	}
+
+	static validateRequest() {
+
+		if(!this.url)
+			return { error: 'Invalid Request. URL is Missing' };
+
+		if(!this.method)
+			return { error: 'Invalid Request. Method is Missing' };
+
+		if(!this.query)
+			return { error: 'Invalid Request. Query is Missing' };
+
+		if(!process.env.GITHUB_TOKEN)
+			return { error: 'Invalid Request. Token is Missing' };
+
+		if(!this.authorization)
+			return { error: 'Invalid Request. Authorization is Missing' };
+	}
+
+	static formatRequest(username) {
+		return {
+			url: this.url,
+			method: this.method,
+			headers: {
+				Authorization: this.authorization
+			},
+			data: {
+				query: this.query,
+				variables: {
+					login: username
+				}
+			}
+		};
+	}
+
+	static validateResponse(response) {
+
+		if(!response?.data)
+			return { error: 'No response from Github' };
+
+
+		if(response.data.errors?.[0].type === ERROR_RATE_LIMITED)
+			return { error: 'Limit Reached' };
+
+		if(response.data.errors)
+			return { error: response.data.errors[0]?.message || 'Cannot fetch data' };
+	}
+
+	static format(responseData) {
+		return responseData;
+	}
+};
---
+++ src/controllers/github-request/queries/user-stats.js
@@ -0,0 +1,34 @@
+'use strict';
+
+module.exports = `
+query userInfo($login: String!) {
+  user(login: $login) {
+	name
+	login
+	contributionsCollection {
+	  totalCommitContributions
+	  restrictedContributionsCount
+	}
+	repositoriesContributedTo(first: 1, contributionTypes: [COMMIT, ISSUE, PULL_REQUEST, REPOSITORY]) {
+	  totalCount
+	}
+	pullRequests(first: 1) {
+	  totalCount
+	}
+	issues(first: 1) {
+	  totalCount
+	}
+	followers {
+	  totalCount
+	}
+	repositories(first: 100, ownerAffiliations: OWNER, isFork: false, orderBy: {direction: DESC, field: STARGAZERS}) {
+	  totalCount
+	  nodes {
+		stargazers {
+		  totalCount
+		}
+	  }
+	}
+  }
+}
+`;
