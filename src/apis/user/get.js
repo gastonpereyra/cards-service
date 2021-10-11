9ce006bb7b4f239c83a1cff2419bef028e38a3da
gastonpereyra <rgpxen@gmail.com>
2021-10-10 21:58:57 -0300

    Updated User API

--- src/apis/user/get.js
+++ src/apis/user/get.js
@@ -6,6 +6,9 @@ const { struct } = require('superstruct');
 const UserStatFetcher = require('../../controllers/github-request/user-stats');
 const CommitsFetcher = require('../../controllers/github-request/commits');
 
+const StatsCard = require('../../controllers/graphics/cards/stats-card');
+const ErrorCard = require('../../controllers/graphics/cards/error');
+
 const logger = require('../../lib/logger').init('User Stats');
 
 const THIRTY_MINUTES = 1800;
@@ -23,14 +26,15 @@ module.exports = class UserGetApi extends API {
 			hide: 'string?',
 			hideTitle: 'string?',
 			hideBorder: 'string?',
-			hideRank: 'string?',
+			hideYear: 'string?',
 			showIcons: 'string?',
-			countPrivate: 'string?',
 			includeAlCommits: 'string?',
 			lineHeight: 'string?',
+			width: 'string?',
 			titleColor: 'string?',
 			iconColor: 'string?',
 			textColor: 'string?',
+			theme: 'string?',
 			backgroundColor: 'string?',
 			cacheSeconds: 'string?'
 		});
@@ -38,54 +42,95 @@ module.exports = class UserGetApi extends API {
 
 	async process() {
 
-		const [userStats, totalCommits] = await Promise.all([
-			UserStatFetcher.get(this.pathIds.username),
-			CommitsFetcher.get(this.pathIds.username)
-		]);
+		const {
+			includeAlCommits,
+			cacheSeconds,
+			colors,
+			...queries
+		} = this.formatQueries();
 
-		if(userStats.error || totalCommits.error)
-			logger.error(userStats.error || totalCommits.error);
+		this.setHeader('Content-Type', 'image/svg+xml');
 
-		const stats = {
-			name: userStats.name || userStats.login,
-			totalPRs: userStats.pullRequests.totalCount,
-			totalCommits: this.countCommits(userStats.contributionsCollection, totalCommits),
-			totalIssues: userStats.issues.totalCount,
-			totalStars: this.countStars(userStats.repositories),
-			contributedTo: userStats.repositoriesContributedTo.totalCount,
-			rank: { level: 'C', score: 0 }
-		};
+		try {
 
-		this.setBody(stats);
+			const [userStats, totalCommits] = await Promise.all([
+				UserStatFetcher.get(this.pathIds.username),
+				includeAlCommits && CommitsFetcher.get(this.pathIds.username)
+			]);
+
+			if(userStats.error) {
+				logger.error(`Error getting User. ${userStats.error}`);
+				throw new Error('Cannot get User Data from Github');
+			}
+
+			if(totalCommits.error)
+				logger.error(`Error getting Extra Commits. ${totalCommits.error}`);
+
+			const stats = this.formatStats(userStats, totalCommits);
+
+			logger.info('User Stats');
+			logger.info(stats);
+
+			const card = new StatsCard(stats, queries, colors);
+
+			this.setHeader('Cache-Control', `public, max-age=${cacheSeconds}`);
+
+			this.setBody(card.render());
+
+		} catch(error) {
+
+			const card = new ErrorCard({ width: queries.width },
+				'Cannot build User\'s Card',
+				error.message
+			);
+
+			this.setBody(card.render());
+		}
 	}
 
 	formatQueries() {
 		return {
-			hide: !this.query.hide ? [] : this.query.hide.split(','),
-			hideTitle: Boolean(this.query.hideTitle),
-			hideBorder: Boolean(this.query.hideBorder),
-			hideRank: Boolean(this.query.hideRank),
-			showIcons: Boolean(this.query.showIcons),
-			countPrivate: Boolean(this.query.countPrivate),
-			includeAlCommits: Boolean(this.query.includeAlCommits),
-			lineHeight: this.query.lineHeight,
+			hide: !this.filters.hide ? [] : this.filters.hide.split(',').filters(stat => StatsCard.statsNames.includes(stat)),
+			hideTitle: Boolean(this.filters.hideTitle || false),
+			hideBorder: Boolean(this.filters.hideBorder || false),
+			hideYear: Boolean(this.filters.hideYear || false),
+			showIcons: Boolean(this.filters.showIcons || true),
+			includeAlCommits: Boolean(this.filters.includeAlCommits || true),
+			lineHeight: Number(this.filters.lineHeight || 25),
+			width: Number(this.filters.width || 460),
 			colors: {
-				title: this.query.titleColor,
-				icon: this.query.iconColor,
-				text: this.query.textColor,
-				background: this.query.backgroundColor
+				...this.filters.titleColor && { title: this.filters.titleColor },
+				...this.filters.iconColor && { icon: this.filters.iconColor },
+				...this.filters.textColor && { text: this.filters.textColor },
+				...this.filters.backgroundColor && { background: this.filters.backgroundColor },
+				...this.filters.theme && { theme: this.filters.theme }
 			},
-			cacheSeconds: Number(this.query.cacheSeconds || THIRTY_MINUTES)
+			cacheSeconds: Number(this.filters.cacheSeconds || THIRTY_MINUTES)
 		};
 	}
 
-	countCommits({ totalCommitContributions = 0, restrictedContributionsCount = 0 }, totalCommits = 0) {
-		return totalCommits + totalCommitContributions + restrictedContributionsCount;
+	formatStats(userStats, totalCommits) {
+
+		const createdToActualDate = new Date() - new Date(userStats.createdAt);
+
+		return {
+			name: userStats.name || userStats.login,
+			totalPRs: userStats.pullRequests?.totalCount || 0,
+			totalCommits: this.countCommits(userStats.contributionsCollection, totalCommits),
+			totalIssues: userStats.issues?.totalCount || 0,
+			totalStars: this.countStars(userStats.repositories),
+			contributedTo: userStats.repositoriesContributedTo?.totalCount || 0,
+			miliSecondsActive: createdToActualDate
+		};
 	}
 
-	countStars({ nodes }) {
-		return nodes.reduce((prev, { stargazers }) => {
-			return prev + stargazers.totalCount;
+	countCommits({ totalCommitContributions = 0, restrictedContributionsCount = 0 } = {}, totalCommits = 0) {
+		return (totalCommits.error ? 0 : totalCommits) + totalCommitContributions + restrictedContributionsCount;
+	}
+
+	countStars({ nodes = [] } = {}) {
+		return nodes.reduce((prev, { stargazers = {} }) => {
+			return prev + stargazers.totalCount || 0;
 		}, 0);
 	}
 };
