9db14a2c2a83e42f6d66b8ca6ead229be7757442
gastonpereyra <rgpxen@gmail.com>
2021-10-10 21:57:10 -0300

    Added Graphics Themes and Animations

---
+++ src/controllers/graphics/themes/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+	dark: {
+		titleColor: '217aff',
+		iconColor: '6cc644',
+		textColor: 'c9d1d9',
+		backgroundColor: '0d1117'
+	},
+	light: {
+		titleColor: '217aff',
+		iconColor: '6cc644',
+		textColor: '0d1117',
+		backgroundColor: 'ffffff'
+	},
+	error: {
+		titleColor: 'FFC220',
+		iconColor: 'FFC220',
+		textColor: 'ffffff',
+		backgroundColor: 'bd2c00'
+	}
+};
