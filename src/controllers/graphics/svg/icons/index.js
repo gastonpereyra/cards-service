0e7f22479595bd66304147fb9b033b6c098af835
gastonpereyra <rgpxen@gmail.com>
2021-10-17 18:17:19 -0300

    Added SVG Icons

--- src/controllers/graphics/svg/icons/index.js
+++ src/controllers/graphics/svg/icons/index.js
@@ -5,15 +5,27 @@ const contributions = require('./contributions');
 const forks = require('./forks');
 const icon = require('./icon');
 const issues = require('./issues');
+const members = require('./members');
+const packages = require('./packages');
+const projects = require('./projects');
 const prs = require('./prs');
+const repositories = require('./repositories');
+const teams = require('./teams');
 const stars = require('./stars');
 
 module.exports = {
 	commits,
 	contributions,
+	discussions: contributions,
 	forks,
 	icon,
 	issues,
+	followers: teams,
+	members,
+	packages,
+	projects,
 	prs,
+	repositories,
+	teams,
 	stars
 };
